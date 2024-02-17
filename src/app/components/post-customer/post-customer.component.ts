202ab3d897559922a9ee0f86fed9ad3a26f4c2d1
Julio Arcos <julio.arcos.salazar@gmail.com>
2024-02-17 15:47:43 -0600

    Modify post-customer.component and add method postCustomer

--- src/app/components/post-customer/post-customer.component.ts
+++ src/app/components/post-customer/post-customer.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../service/customer.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-customer',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './post-customer.component.html',
   styleUrl: './post-customer.component.css'
 })
 export class PostCustomerComponent {
+
+  postCustomerForm!: FormGroup;
+
+  constructor(private customerService: CustomerService, private fb: FormBuilder){
+
+  }
+
+  ngOnInit(){
+
+    this.postCustomerForm = this.fb.group({
+
+      name: [null, Validators.required],
+      email: [null, Validators.required, Validators.email],
+      phone: [null, Validators.required],
+    })
+  }
+
+  
+  postCustomer(){
+
+    console.log(this.postCustomerForm.value)
+
+   /* this.customerService.postCustomer().subscribe((res) => {
+
+      console.log(res);
+
+    })
+  }
+*/
+  }
 
 }
