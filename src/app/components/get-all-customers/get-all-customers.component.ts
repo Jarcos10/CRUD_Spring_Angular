089aa58374a3e5b100086f005225038156cc0cef
Julio Arcos <julio.arcos.salazar@gmail.com>
2024-02-24 14:53:29 -0600

    Add imports and declare array customers

--- src/app/components/get-all-customers/get-all-customers.component.ts
+++ src/app/components/get-all-customers/get-all-customers.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CustomerService } from '../../service/customer.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-get-all-customers',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './get-all-customers.component.html',
   styleUrl: './get-all-customers.component.css',
 })
 export class GetAllCustomersComponent {
+
+  customers: any = [];
 
   constructor(private service: CustomerService) {}
 
@@ -17,10 +21,11 @@ export class GetAllCustomersComponent {
   }
 
   getAllCustomers(){
-    
-    this.service.getAllCustomers().subscribe(res => {
 
+    this.service.getAllCustomers().subscribe(res => {
       console.log(res);
+      
+      this.customers = res;
 
     })
   }
