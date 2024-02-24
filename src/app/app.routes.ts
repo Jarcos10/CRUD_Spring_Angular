2a63c9fd9abad7f2bca43ab1e5d3b081dc7af420
Julio Arcos <julio.arcos.salazar@gmail.com>
2024-02-24 13:37:47 -0600

    Add path /customers method getAllCustomers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PostCustomerComponent } from './components/post-customer/post-customer.component';
+import { GetAllCustomersComponent } from './components/get-all-customers/get-all-customers.component';
 
 export const routes: Routes = [
 
-    {path: "customer", component: PostCustomerComponent}
+    {path: "customer", component: PostCustomerComponent},
+    {path: "", component: GetAllCustomersComponent},
 ];
